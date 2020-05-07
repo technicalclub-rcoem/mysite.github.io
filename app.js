319cb7ccf646f736e0902c80ad8ffd90481ded40
technicalclub-rcoem <technicalclub-rcoem>
2020-05-08 00:28:05 +0530

    rotation modified to offsetX by 20, tap enabled

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ Stage(function(stage){
 
 	var number=0;
 	stage.viewbox(50,50).pin('handle',-0.5);
-	var mark =Stage.image('b').appendTo(stage).pin('handle',0.5);
+	var mark =Stage.image('b').appendTo(stage).pin({
+		'handle':0.5,
+		'offsetX':-10
+	});
 
 
 	mark.on('click',function(){
-
+		if(number==0){
 		// console.log("num :"+number);
-		mark.tween().ease('in-out').pin('rotation',Math.PI *2);
+			mark.tween().ease('in-out').pin('offsetX',20);
 		// number++;
+			number=1;
+		}
+
+		else{
+			mark.tween().ease('in-out').pin('offsetX',-20);
+		// number++;
+			number=00;
+		}
 	});
 
 });
