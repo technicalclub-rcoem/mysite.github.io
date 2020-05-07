fb2135093824471770268a666976823f6706f446
technicalclub-rcoem <technicalclub-rcoem>
2020-05-07 23:37:38 +0530

    a x mark is added on click event

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 Stage(function(stage){
 
 	var number=0;
-	stage.viewbox(50,50);
+	stage.viewbox(50,50).pin('handle',-0.5);
+
 
 	stage.on('click',function(){
 
-		console.log("num :"+number);
-		number++;
+		// console.log("num :"+number);
+
+		Stage.image('b').appendTo(stage).pin('handle',0.5);
+		// number++;
 	});
 
 });
 
-Stage(
+Stage({
 	textures :{
-		
+		'b' : Stage.canvas(function(ctx){
+			ratio = 20;
+			this.size(10,10,ratio);
+			ctx.scale(ratio,ratio);
+
+			ctx.moveTo(0,0);
+			ctx.lineTo(10,10);
+
+			ctx.moveTo(10,0);
+			ctx.lineTo(0,10);
+			ctx.strokeWidth = 1;
+			ctx.stroke();
+		})
 	}
-);
+});
